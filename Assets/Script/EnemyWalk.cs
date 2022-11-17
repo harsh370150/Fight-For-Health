0aa96830bcfaa2e8edf755b92baf974eac9c3252
harsh370150 <68119867+harsh370150@users.noreply.github.com>
2022-11-17 14:58:59 +0530

    Add files via upload

---
+++ Assets/Script/EnemyWalk.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWalk : MonoBehaviour
+{
+    public GameObject DownCamera;
+    public GameObject UpCamera;
+    public GameObject Enemy;
+    void Update()
+    {
+        DownCamera.SetActive(true);
+        UpCamera.SetActive(false);
+        if(Enemy.transform.position.z < 225)
+        {
+            transform.Translate(0, 0, 10 * Time.deltaTime);
+            DownCamera.SetActive(false);
+            UpCamera.SetActive(true);
+        }
+    }
+}
